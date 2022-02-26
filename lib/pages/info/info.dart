2ce0808819c181591b4d8f8dfee237cc6634c8e3
Christian Pauly <christian-pauly@posteo.de>
2022-02-26 14:43:59 +0100

    feat: Add info page

--- lib/pages/info/info.dart
+++ lib/pages/info/info.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class InfoPage extends StatelessWidget {
   const InfoPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    const textStyle = TextStyle(fontSize: 16);
+    const headerStyle = TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+    return Scaffold(
+      appBar:
+          AppBar(title: Text(L10n.of(context)!.learnJapaneseLetteringSystems)),
+      body: ListView(
+        padding: const EdgeInsets.all(12),
+        children: [
+          SelectableText(
+            L10n.of(context)!.learnJapaneseLetteringSystemsDesc,
+            style: textStyle,
+          ),
+          const SizedBox(height: 12),
+          SelectableText(
+            L10n.of(context)!.hiragana,
+            style: headerStyle,
+          ),
+          const SizedBox(height: 12),
+          SelectableText(
+            L10n.of(context)!.hiraganaDesc,
+            style: textStyle,
+          ),
+          const SizedBox(height: 12),
+          SelectableText(
+            L10n.of(context)!.katakana,
+            style: headerStyle,
+          ),
+          const SizedBox(height: 12),
+          SelectableText(
+            L10n.of(context)!.katakanaDesc,
+            style: textStyle,
+          ),
+          const SizedBox(height: 12),
+          SelectableText(
+            L10n.of(context)!.radicals,
+            style: headerStyle,
+          ),
+          const SizedBox(height: 12),
+          SelectableText(
+            L10n.of(context)!.radicalsDesc,
+            style: textStyle,
+          ),
+          const SizedBox(height: 12),
+          SelectableText(
+            L10n.of(context)!.kanji,
+            style: headerStyle,
+          ),
+          const SizedBox(height: 12),
+          SelectableText(
+            L10n.of(context)!.kanjiDesc,
+            style: textStyle,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/overview/overview_view.dart
+++ lib/pages/overview/overview_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:funny_kanji/pages/overview/overview.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:yaru_icons/yaru_icons.dart';
 
 class OverviewPageView extends StatelessWidget {
   final OverviewController controller;
@@ -18,66 +19,77 @@ class OverviewPageView extends StatelessWidget {
           title: Text(L10n.of(context)!.hiragana),
           subtitle: Text(L10n.of(context)!.countEntries('199')),
           onTap: controller.goToHiraganaViewer,
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('カ')),
           title: Text(L10n.of(context)!.katakana),
           subtitle: Text(L10n.of(context)!.countEntries('105')),
           onTap: controller.goToKatakanaViewer,
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('ら')),
           title: Text(L10n.of(context)!.radicals),
           subtitle: Text(L10n.of(context)!.countEntries('232')),
           onTap: controller.goToRadicalsViewer,
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('一')),
           title: Text(L10n.of(context)!.kanjiLevel('1')),
           subtitle: Text(L10n.of(context)!.countEntries('267')),
           onTap: () => controller.goToKanjiViewer(1),
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('所')),
           title: Text(L10n.of(context)!.kanjiLevel('2')),
           subtitle: Text(L10n.of(context)!.countEntries('267')),
           onTap: () => controller.goToKanjiViewer(2),
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('典')),
           title: Text(L10n.of(context)!.kanjiLevel('3')),
           subtitle: Text(L10n.of(context)!.countEntries('267')),
           onTap: () => controller.goToKanjiViewer(3),
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('述')),
           title: Text(L10n.of(context)!.kanjiLevel('4')),
           subtitle: Text(L10n.of(context)!.countEntries('267')),
           onTap: () => controller.goToKanjiViewer(4),
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('訳')),
           title: Text(L10n.of(context)!.kanjiLevel('5')),
           subtitle: Text(L10n.of(context)!.countEntries('267')),
           onTap: () => controller.goToKanjiViewer(5),
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('儀')),
           title: Text(L10n.of(context)!.kanjiLevel('6')),
           subtitle: Text(L10n.of(context)!.countEntries('267')),
           onTap: () => controller.goToKanjiViewer(6),
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('拘')),
           title: Text(L10n.of(context)!.kanjiLevel('7')),
           subtitle: Text(L10n.of(context)!.countEntries('267')),
           onTap: () => controller.goToKanjiViewer(7),
+          trailing: const Icon(YaruIcons.go_next),
         ),
         ListTile(
           leading: const CircleAvatar(child: Text('窃')),
           title: Text(L10n.of(context)!.kanjiLevel('8')),
           subtitle: Text(L10n.of(context)!.countEntries('267')),
           onTap: () => controller.goToKanjiViewer(8),
+          trailing: const Icon(YaruIcons.go_next),
         ),
       ]),
     );
