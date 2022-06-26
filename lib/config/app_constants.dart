b22b949cf248bcb889ce9f1c847e8192e36dbe4d
Christian Pauly <christian-pauly@posteo.de>
2022-06-26 16:33:47 +0200

    chore: Bump version

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class AppConstants {
-  static const String appName = 'Fun With 32Kanji';
+  static const String appName = 'Fun With Kanji';
   static const String appId = 'krillefear.funnykanji';
   static const String website = 'https://gitlab.com/KrilleFear/funny-kanji';
 }
